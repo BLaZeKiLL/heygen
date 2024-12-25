1856939a60e6c21ee3e575f24a6d2c8789db3945
Devashish Lal <laldevashish@gmail.com>
2024-12-25 01:10:22 -0800

    cli app created
    Signed-off-by: Devashish Lal <laldevashish@gmail.com>

--- heygen-app/src/routes/+page.svelte
+++ heygen-app/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="h-screen p-4 sm:p-16 flex flex-col place-content-center items-center gap-4">
+	<h1 class="h1">HeyGen Status App</h1>
+	<!-- <a href="/intro" class="btn preset-filled-success-500">Play</a> -->
+</div>
